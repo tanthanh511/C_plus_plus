030f3f6cfe7799503893666ad56d589e1edd570d
Huỳnh Tấn Thanh <117253637+tanthanh511@users.noreply.github.com>
2024-03-07 15:41:29 +0700

    fix menu

--- basic/syntaxC++/menu/menu.cpp
+++ basic/syntaxC++/menu/menu.cpp
@@ -1,38 +1,57 @@
-#include <iostream>
+ï»¿#include <iostream>
 using namespace std;
 void menu();
-int chonMenu(int menu);
-int xuatMenu();
+int chonMenu();
+void xuatMenu(int n, int chon);
 
 
 int main()
 {
+    int chon, n;
+    cout << "nhap n: ";
+    cin >> n;
+    do {
+        chon = chonMenu();
+        xuatMenu(n, chon);
+    } while (1);
   
 }
 
 void menu() {
     cout << "Bang menu\n";
-    cout << "Thoát\n";
-    cout << "Chuc nang 1\n";
-    cout << "Chuc nang 2\n";
-    cout << "Chuc nang 3\n";
-    cout << "Chuc nang 4\n";
+    cout << "1. Chuc nang 1\n";
+    cout << "2. Chuc nang 2\n";
+    cout << "3. Chuc nang 3\n";
+    cout << "4. thoat\n";
 }
 
-int chonMenu(int menu) {
-    int Chon;
-    cout << "Please enter function:";
-    cin >> Chon;
-    return Chon;
+int chonMenu() {
+    int chon;
+    for (;;) {
+        menu();
+        cout << "nhap tu 1 - 4: ";
+        cin >> chon;
+        if (1 <= chon && chon <= 4) {
+            break;
+        } 
+    }
+    return chon;
 }
 
-int xuatMenu() {
-    int menu = 0;
-
-    switch (menu)
+void xuatMenu(int n, int chon) {
+    switch (chon)
     {
-
-
+    case 1: 
+        cout << "hihi";
+        break;
+    case 2:
+        cout << "hihi";
+        break;
+    case 3:
+        cout << "hihi";
+        break;
+    case 4:
+        exit(1);
     }
 }
 
